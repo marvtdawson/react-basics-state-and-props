c17f7646049060f6c8e75cc41107d2e790cfa8f6
Marvin Dawson <marvintdawson@gmail.com>
2021-06-10 08:14:10 -0500

    updating the app

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
                 <div className="App">
                     <Header />
                     <Home />
-                    <Footer/>
+                    <Footer />
                 </div>
             </BrowserRouter>
         );
--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,42 +1,50 @@
-import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {Col, Container, Row} from "react-bootstrap";
 import './Header.scss';
-// import About from '../../../containers/CorePages/About/About';
 
-import marvinPortrait from '../../../assets/img/selfPortrait/marvin_2019_140_x_140.png';
+// import About from '../../../containers/CorePages/About/About';
+// import marvinPortrait from '../../../assets/img/selfPortrait/marvin_2019_140_x_140.png';
 
 class Header extends Component {
     render() {
         return (
-            <header className="header">
-                <div className="header__logo-wrapper">
-                    Marvin T. Dawson
-                    {/*<img className="header__logo-wrapper__img"
+
+            <Container fluid className="header">
+                <Row>
+                    <Col xs={6}>
+                        <div className="header__logo-wrapper">
+                            Marvin T. Dawson
+                            {/*<img className="header__logo-wrapper__img"
                          src={ marvinPortrait } alt="Marvin's Portrait"/>*/}
-                </div>
-                <nav className="header__nav-wrapper">
-                    <ul>
-                        <li><Link to="/">HOME</Link></li>
-                        |
-                        <li><a href="/about">ABOUT</a></li>
-                        |
-                        <li>
-                            <Link to={{
-                                    pathname: '/person'
-                                }}>PERSON
-                            </Link>
-                        </li>
-                        |
-                        <li><Link to={{
-                            pathname: '/blog'
-                        }}>BLOG</Link></li> |
-                        <li><Link to={{
-                            pathname: './contact'
-                        }}>CONTACT</Link></li>
-                    </ul>
-                </nav>
-            </header>
+                        </div>
+                    </Col>
+                    <Col xs={6}>
+                        <Row>
+                            <Col><Link to={{pathname: '/'}}>HOME</Link></Col>
+                            <Col><Link to={{pathname: '/about'}}>ABOUT</Link></Col>
+                            <Col><Link to={{pathname: '/person'}}>PERSON</Link></Col>
+                            <Col><Link to={{pathname: '/blog'}}>BLOG</Link></Col>
+                            <Col><Link to={{pathname: '/contact'}}>CONTACT</Link></Col>
+                        </Row>
+                        {/*<nav>
+                            <ul>
+                                <li></li>
+                                |
+                                <li></li>
+                                |
+                                <li></li>
+                                |
+                                <li></li>
+                                |
+                                <li></li>
+                            </ul>
+                        </nav> */}
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
+
 export default Header;
